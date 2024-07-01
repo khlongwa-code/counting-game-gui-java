cfa80403ae6250a222a5b8dc0994a054fc0f44d8
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-07-01 11:07:28 +0200

    added score panel

--- CountingGame.java
+++ CountingGame.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,5 +30,32 @@ public class CountingGame {
         frame.add(buttonPanel);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private JPanel getScorePanel() {
+        JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.X_AXIS));
+        JLabel instruction = new JLabel("Score:");
+        JButton reset = new JButton("Reset");
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                score.setText("0");
+                nextCorrect = 1;
+
+                for (int i = 0; i < 9; i++) {
+                    buttons[i].setBackground(Color.WHITE);
+                }
+                randomButtons();
+            }
+        });
+        scorePanel.add(instruction);
+        scorePanel.add(score);
+        scorePanel.add(reset);
+        instruction.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        score.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 50));
+        scorePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 40));
+
+        return scorePanel;
     }
 }
